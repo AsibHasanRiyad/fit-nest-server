25a1f994582aeaf3494905f90bc886bc9c0193cd
Asib Hasan Riyad <asibhasanriyad@gmail.com>
2024-07-17 11:51:01 +0600

    some changes in server and config file

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,6 @@ import dotenv from "dotenv";
 dotenv.config();
 export default {
   port: process.env.PORT,
-  db_url: process.env.DB_URL,
+  db_url: process.env.DATABASE_URL,
   node_env: process.env.NODE_ENV,
 };
