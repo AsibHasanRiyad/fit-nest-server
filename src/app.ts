6b0692dd20261bd7cfa7852f24bfee3c6b5c32c1
Asib Hasan Riyad <asibhasanriyad@gmail.com>
2024-09-25 11:09:45 +0600

    completed

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,12 @@ const app = express();
 // parser
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://fitnest-seven.vercel.app"],
+    credentials: true,
+  })
+);
 
 //application route
 app.use("/api/v1", router);
